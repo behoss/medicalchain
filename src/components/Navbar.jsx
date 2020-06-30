8a6e81501f38d6db80e8a36dd854dff52751806c
Behzad Hosseini <b.hosseini@hotmail.com>
2020-06-30 19:29:00 +0100

    fixed typo in navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -13,7 +13,7 @@ export const Navbar = () => {
             <Link to="/">Home</Link>
           </li>
           <li>
-            <NavLink to="/patients">Patiens</NavLink>
+            <NavLink to="/patients">Patients</NavLink>
           </li>
           <li>
             <NavLink to="/about">About</NavLink>
